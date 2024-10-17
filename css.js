c7f471e0410256f253d26835c47d3efdf349e531
MoriumAkter123 <morium2312091068@diu.edu.bd>
2024-10-17 08:52:34 +0600

    Review-Class

---
+++ css.js
@@ -0,0 +1,7 @@
+let totalDonation_money = 5100000;
+var added_international_organaization = 2000000;
+var remaaining_balnace = totalDonation_money + added_international_organaization;
+console.log("result of remaaining_balnace ", remaaining_balnace  );
+var families = 90;
+var divided_balance = remaaining_balnace / families;
+console.log("Each family will get money = ", divided_balance);
